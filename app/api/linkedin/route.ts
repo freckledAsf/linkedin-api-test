82ecc9b588dbe77494fcc5c7fceba9d41b38e2fd
Victor Tamayo Viera <106868505+freckledAsf@users.noreply.github.com>
2023-12-28 19:42:33 +0100

    feat: update linkedin endpoint

--- app/api/linkedin/route.ts
+++ app/api/linkedin/route.ts
@@ -1,24 +1,28 @@
+import { Profile } from "@/types";
 import axios from "axios";
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request) {
-  const options = {
-    method: "GET",
-    url: "https://fresh-linkedin-profile-data.p.rapidapi.com/get-linkedin-profile",
-    params: {
-      linkedin_url: "https://www.linkedin.com/in/yifenghan/",
-      include_skills: "true",
-    },
-    headers: {
-      "X-RapidAPI-Key": process.env.XRAPID_API_KEY,
-      "X-RapidAPI-Host": "fresh-linkedin-profile-data.p.rapidapi.com",
-    },
-  };
-
+export async function POST(req: Request) {
   try {
+    const { linkedin_url } = await req.json();
+    if (!linkedin_url) throw Error("Linkedin url not specified");
+
+    const options = {
+      method: "GET",
+      url: "https://fresh-linkedin-profile-data.p.rapidapi.com/get-linkedin-profile",
+      params: {
+        linkedin_url,
+        include_skills: "true",
+      },
+      headers: {
+        "X-RapidAPI-Key": process.env.XRAPID_API_KEY,
+        "X-RapidAPI-Host": "fresh-linkedin-profile-data.p.rapidapi.com",
+      },
+    };
+
     const res = await axios.request(options);
-    console.log(res.data);
-    return NextResponse.json(res.data);
+    const data: Profile = res.data.data;
+    return NextResponse.json(data);
   } catch (error) {
     console.log(error);
     return new NextResponse("Internal Error", { status: 500 });
